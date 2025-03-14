90484792d7b10d4eed0e03b0cd4e6c6f33148e04
AKNov30 <amonthjp@gmail.com>
2025-03-14 17:32:25 +0700

    s

--- lib/screen/signup.dart
+++ lib/screen/signup.dart
@@ -36,124 +36,127 @@ class _SignupState extends State<Signup> {
           ),
           centerTitle: true,
         ),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Container(
-              margin: EdgeInsets.all(20),
-              child: Column(
-                children: [
-                  Text(
-                    "Please enter the information \n           below to access.",
-                    style: GoogleFonts.outfit(fontSize: 16),
-                  ),
-                  SizedBox(height: 25),
-                  Image.asset(
-                    "assets/images/Ion Signup.png",
-                    width: 98,
-                    height: 98,
-                  ),
-                  SizedBox(height: 30),
-                  Padding(
-                    padding: EdgeInsets.all(5),
-                    child: Form(
-                      key: formkey,
-                      child: Column(
-                        children: [
-                          TextFormField(
-                            // maxLength: 20,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(15),
+        body: Container(
+          // decoration: DecorationImage(image: Image.asset("assets/images/signup.png"),),
+          child: SingleChildScrollView(
+            child: Center(
+              child: Container(
+                margin: EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Text(
+                      "Please enter the information \n           below to access.",
+                      style: GoogleFonts.outfit(fontSize: 16),
+                    ),
+                    SizedBox(height: 25),
+                    Image.asset(
+                      "assets/images/Ion Signup.png",
+                      width: 98,
+                      height: 98,
+                    ),
+                    SizedBox(height: 30),
+                    Padding(
+                      padding: EdgeInsets.all(5),
+                      child: Form(
+                        key: formkey,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              // maxLength: 20,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(15),
+                                  ),
                                 ),
-                              ),
-                              label: Text(
-                                "First name",
-                                style: TextStyle(fontSize: 15),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          TextFormField(
-                            // maxLength: 20,
-                            controller: userLastname,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(15),
-                                ),
-                              ),
-                              label: Text(
-                                "Last name",
-                                style: TextStyle(fontSize: 15),
-                              ),
-                            ),
-                            validator: (value) {
-                              if (value!.isEmpty) {
-                                return 'Please enter your username';
-                              }
-                              return null;
-                            },
-                          ),
-                          SizedBox(height: 10),
-                          TextFormField(
-                            keyboardType: TextInputType.emailAddress,
-                            // maxLength: 20,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(15),
-                                ),
-                              ),
-                              label: Text(
-                                "Email",
-                                style: TextStyle(fontSize: 15),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          TextFormField(
-                            obscureText: true,
-                            // keyboardType: TextInputType.visiblePassword,
-                            // maxLength: 20,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(15),
-                                ),
-                              ),
-                              label: Text(
-                                "Password",
-                                style: TextStyle(fontSize: 15),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 30),
-                          SizedBox(
-                            width: double.infinity,
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Color(0xff00503E),
-                                padding: EdgeInsets.all(15),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(15),
-                                ),
-                              ),
-                              onPressed: () {},
-                              child: Text(
-                                "SIGN UP",
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.white,
+                                label: Text(
+                                  "First name",
+                                  style: TextStyle(fontSize: 15),
                                 ),
                               ),
                             ),
-                          ),
-                        ],
+                            SizedBox(height: 10),
+                            TextFormField(
+                              // maxLength: 20,
+                              controller: userLastname,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(15),
+                                  ),
+                                ),
+                                label: Text(
+                                  "Last name",
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                              ),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Please enter your username';
+                                }
+                                return null;
+                              },
+                            ),
+                            SizedBox(height: 10),
+                            TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              // maxLength: 20,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(15),
+                                  ),
+                                ),
+                                label: Text(
+                                  "Email",
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            TextFormField(
+                              obscureText: true,
+                              // keyboardType: TextInputType.visiblePassword,
+                              // maxLength: 20,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(15),
+                                  ),
+                                ),
+                                label: Text(
+                                  "Password",
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 30),
+                            SizedBox(
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Color(0xff00503E),
+                                  padding: EdgeInsets.all(15),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                ),
+                                onPressed: () {},
+                                child: Text(
+                                  "SIGN UP",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
